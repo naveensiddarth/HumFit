5bc8c7f7c45a2cedb2408776908dbb9e181752d7
naveensiddarth <77882107+naveensiddarth@users.noreply.github.com>
2021-01-28 10:31:18 +0530

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -5,18 +5,19 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+import ast
+from rasa_sdk.events import SlotSet
+from rasa_sdk.forms import FormAction
+
+class RecipeForm(FormAction):
+
+     def name(self) -> Text:
+#         return ""
 #
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
